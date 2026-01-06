e64b407ed057dacbd9b7ae1c9cc2433be98e0840
SKRD <shankaarshiva@gmail.com>
2026-01-07 00:39:20 +0800

    Completed Chapter 1 of UCSandiego 202 course

--- edx/Graph-Algorithms/week1_decomposition1_starters/2_connected_components/connected_components.py
+++ edx/Graph-Algorithms/week1_decomposition1_starters/2_connected_components/connected_components.py
@@ -1,21 +1,36 @@
-#Uses python3
-
-import sys
-
-
 def number_of_components(adj):
     result = 0
-    #write your code here
+    n = len(adj)  # n = number of vertices
+    visited = [False] * n  # Only initialise once
+
+    def dfs(x):
+        visited[x] = True  # Mark the starting vertex as explored
+        for neighbour in adj[x]:
+            if not visited[neighbour]:  # not False = True
+                dfs(neighbour)
+        return 0  # Dead end
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            result += 1
+
     return result
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+
+if __name__ == "__main__":
+    input = """
+        4 2
+        1 2
+        3 2
+        """
+
+    data = list(map(int, input.strip().split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
-    for (a, b) in edges:
+    for a, b in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
     print(number_of_components(adj))
--- edx/Graph-Algorithms/week2_decomposition2_starters/1_acyclicity/acyclicity.py
+++ edx/Graph-Algorithms/week2_decomposition2_starters/1_acyclicity/acyclicity.py
@@ -1,4 +1,4 @@
-#Uses python3
+# Uses python3
 
 import sys
 
@@ -6,13 +6,14 @@ import sys
 def acyclic(adj):
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
-    for (a, b) in edges:
+    for a, b in edges:
         adj[a - 1].append(b - 1)
     print(acyclic(adj))
